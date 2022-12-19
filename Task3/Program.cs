2426bd360c27e1fee8f2333b7f3068f8c5414042
Anna <annprokopyeva@gmail.com>
2022-12-19 10:56:43 +0800

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+//Считываем данные с консоли
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (num1Line != null && num2Line != null)
+{
+    //Парсим введеное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    if (num2 * num2 == num1)
+    {
+        Console.WriteLine("Первое число квадрат второго!");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не квадрат второго!");
+    }
+}
+
