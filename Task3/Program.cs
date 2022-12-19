e5dbbe1ea14be6491131c9318a5c2b59e9b1d519
Anna <annprokopyeva@gmail.com>
2022-12-19 11:34:49 +0800

    Task3 edited

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,23 +1,50 @@
 ﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
 
 //Считываем данные с консоли
-string? num1Line = Console.ReadLine();
-string? num2Line = Console.ReadLine();
+string? inputLine = Console.ReadLine();
 
 //Проверяем, чтобы данные были не пустыми
-if (num1Line != null && num2Line != null)
-{
-    //Парсим введеное число
-    int num1 = int.Parse(num1Line);
-    int num2 = int.Parse(num2Line);
+if (inputLine != null)
+{   
+    //Способ №1
+    //Создаем масив с днями недели
+    //string[] dayOfWeek = new string[7];
+    //dayOfWeek[0] = "Понедельник";
+    //dayOfWeek[1] = "Вторник";
+    //dayOfWeek[2] = "Среда";
+    //dayOfWeek[3] = "Четверг";
+    //dayOfWeek[4] = "Пятница";
+    //dayOfWeek[5] = "Суббота";
+    //dayOfWeek[6] = "Воскресенье";
 
-    if (num2 * num2 == num1)
-    {
-        Console.WriteLine("Первое число квадрат второго!");
-    }
-    else
-    {
-        Console.WriteLine("Первое число не квадрат второго!");
-    }
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим название дня недели по введенному номеру
+    //string outDayOfWeek = dayOfWeek[inputNumber-1];
+
+
+    //Способ №2
+    //string outDayOfWeek = string.Empty;
+
+    //switch (inputNumber)
+    //{
+        //case 1: outDayOfWeek = "Понедельник"; break;
+        //case 2: outDayOfWeek = "Вторник"; break;
+        //case 3: outDayOfWeek = "Среда"; break;
+        //case 4: outDayOfWeek = "Четверг"; break;
+        //case 5: outDayOfWeek = "Пятница"; break;
+        //case 6: outDayOfWeek = "Суббота"; break;
+        //case 7: outDayOfWeek = "Воскресенье"; break;
+        //default: outDayOfWeek = "Это не день недели!"; break;
+
+    //}
+
+
+    //Способ №3
+    string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+
+    //Выводим данные в консоль
+    Console.WriteLine(outDayOfWeek);
 }
 
