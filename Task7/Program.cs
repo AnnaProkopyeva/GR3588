fe4805db15dd7e87d99ffe9b21c9ac2e3b570a41
Anna <annprokopyeva@gmail.com>
2022-12-19 11:51:02 +0800

    add Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    int lastDigit = inputNumber % 10;
+
+    //Выводим данные в консоль
+    Console.WriteLine(lastDigit);
+}
