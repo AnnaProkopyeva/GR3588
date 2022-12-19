c02b704a238f2203433444b32891a019ed377a66
Anna <annprokopyeva@gmail.com>
2022-12-19 10:45:40 +0800

    add Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача на возведение числа в квадрат
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    //Парсим введеное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим квадрат числа
+    //Способ №1
+    //int outNumber = inputNumber * inputNumber;
+    //Способ №2 (встроенная формула)
+    //int outNumber = (int)Math.Pow(inputNumber, 2); 
+
+    //Выводим данные в консоль
+    //Для способа №1 и №2
+    //Console.WriteLine(outNumber);
+    Console.WriteLine(Math.Pow(int.Parse(inputLine), 2)); //(Способ №3, используется без нахождения квадрата)
+}
