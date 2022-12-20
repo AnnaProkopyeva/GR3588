ea3c4cfb11326cb2ef54336826a0b2325bc99762
Anna <annprokopyeva@gmail.com>
2022-12-20 16:22:18 +0800

    add Task8 (homework)

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    //Парсим введеное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Вводим новые переменные
+    int startNumber = 2;
+    string result = string.Empty;
+
+    while (startNumber <= inputNumber)
+    {
+        result = result + startNumber;
+        startNumber = startNumber + 2;
+    }
+
+    Console.WriteLine(result);
+    
+}
