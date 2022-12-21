5ef94e2d7e82516858eecc21ee5cf976a7c3ed60
Anna <annprokopyeva@gmail.com>
2022-12-21 21:46:46 +0800

    add solution for task 9,11,12,14,16

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+//Вариант №1
+// System.Random numberSintezator = new System.Random();   // создаем экземпляр класса рандомайзер
+// int num = numberSintezator.Next(100,1000);             // получаем случайное число
+// Console.WriteLine (num);
+// Console.WriteLine((num/100)*10 + num%10);
+
+//Вариант №2
+System.Random random = new System.Random();
+
+int randomNumber = random.Next(100, 1000);
+
+Console.WriteLine("число: " + randomNumber);
+
+string value = randomNumber.ToString();
+
+char[] numberArray = value.ToCharArray();
+
+Console.WriteLine(numberArray[0] + "" + numberArray[2]);
---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+void Test(int inputNumber1, int inputNumber2)
+{
+    bool result = (inputNumber1 / inputNumber2 == inputNumber2);
+    if (result == true) Console.WriteLine(" Число " + inputNumber1 + " является квадратом числа " + inputNumber2);
+    else Console.WriteLine(" Число " + inputNumber1 + " не является квадратом числа " + inputNumber2);
+}
+
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+Test(number1, number2);
+Test(number2, number1);
+
