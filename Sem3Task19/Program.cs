fb657e66f727ad7467995cdc5fe3661e363073fc
Anna <annprokopyeva@gmail.com>
2022-12-26 07:06:46 +0800

    Homework from 3 seminar has done

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int inputNumber = ReadData("Введите число: ");
+bool result = PalinTest(inputNumber);
+PrintData(result);
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод проверки числа на полиндромность
+bool PalinTest(int inputNumber)
+{
+    bool result = false;
+    result = (inputNumber / 10000 == inputNumber % 10) && ((inputNumber / 1000) % 10 == (inputNumber / 10) % 10);
+    return result;
+}
+
+//Метод вывода данных
+void PrintData(bool result)
+{
+    Console.WriteLine(result);
+}
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int X1 = ReadData("Введите координату x1: ");
+int Y1 = ReadData("Введите координату y1: ");
+int Z1 = ReadData("Введите координату z1: ");
+int X2 = ReadData("Введите координату x2: ");
+int Y2 = ReadData("Введите координату y2: ");
+int Z2 = ReadData("Введите координату z2: ");
+
+double result = CalculateLenght(X1, X2, Y1, Y2, Z1, Z2);
+
+PrintData("Расстояние между точками: ", result);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод находит расстояние между точками на плоскости
+double CalculateLenght(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+    double distance = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1-z2), 2));
+    return distance;
+}
+
+//Метод выводит данные пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
