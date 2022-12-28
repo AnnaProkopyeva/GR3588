a1778167e5fd078a33c89c71f553d0b4db3cb3ff
Anna <annprokopyeva@gmail.com>
2022-12-28 19:35:41 +0800

    Add solution for task 24, 26, 28, 30

---
+++ Task26/Program.cs
@@ -0,0 +1,63 @@
+﻿// -------------------------------------------------
+// Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе.
+// -------------------------------------------------
+
+
+// Метод ввода данных от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода данных
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод вычисления через логарифм (самый медленный вариант)
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num) + 1;
+    return count;
+}
+
+// Метод через вычесления длины
+int VariantString(int num)
+{
+    int count = num.ToString().Length;
+    return count;
+}
+
+// Метод через цикл
+int VariantSimple(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А: ");
+
+DateTime d1 = DateTime.Now;
+int length1 = VariantLog(number);
+Console.WriteLine(DateTime.Now - d1);
+
+DateTime d2 = DateTime.Now;
+int length2 = VariantString(number);
+Console.WriteLine(DateTime.Now - d2);
+
+DateTime d3 = DateTime.Now;
+int length3 = VariantSimple(number);
+Console.WriteLine(DateTime.Now - d3);
+
+
+PrintData("Количество цифр в числе равно: " + length1);
+PrintData("Количество цифр в числе равно: " + length2);
+PrintData("Количество цифр в числе равно: " + length3);
---
+++ Task28/Program.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------
+// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// -------------------------------------------------
+
+
+using System.Numerics;
+
+// Метод ввода данных от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода данных
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+BigInteger CalcFactor(int num)
+{
+    BigInteger count = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        count *= i;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А: ");
+
+BigInteger length = CalcFactor(number);
+
+PrintData("Факториал равно: " + length);
