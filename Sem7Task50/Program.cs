c8566f8ada54670bd997de2710c89bcdd3993cc1
Anna <annprokopyeva@gmail.com>
2023-01-09 21:51:23 +0800

    Homework from 7 seminar has done

---
+++ Sem7Task50/Program.cs
@@ -0,0 +1,85 @@
+﻿// ------------------------------------------------------------------------------------------
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание, что такого элемента нет.
+// ------------------------------------------------------------------------------------------
+
+int row = ReadData("Введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+Console.WriteLine("Сгенерированный массив: ");
+Print2DArrayColor(arr2D);
+Console.WriteLine();
+int findRow = ReadData("Введите запрашиваемую строку: ");
+int findColumn = ReadData("Введите запрашиваемый столбец: ");
+int index = SerachElement(arr2D, findRow - 1, findColumn - 1);
+Console.WriteLine();
+PrintData("Результат: ", index);
+
+// Метод ввода данных пользователем
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод генерации и заполнение двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+// Метод поиска заданного элемента
+int SerachElement(int[,] arr, int findRo, int findCol)
+{
+    int elem = -1;
+    if (findRo < arr.GetLength(0))
+    {
+        if (findCol < arr.GetLength(1))
+        {
+            elem = arr[findRo, findCol];
+        }
+    }
+    return elem;
+}
+
+// Метод печати цветного двумерного массива
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод вывода данных
+void PrintData(string msg, int result)
+{
+    Console.WriteLine(msg + result);
+}
+
+
+
+
---
+++ Sem7Task52/Program.cs
@@ -0,0 +1,91 @@
+﻿// ------------------------------------------------------------
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// ------------------------------------------------------------
+
+int row = ReadData("Введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+Console.WriteLine("Сгенерированный массив: ");
+Print2DArrayColor(arr2D);
+Console.WriteLine();
+Console.WriteLine("Среднее арифмитическое каждого столбца равен: ");
+PrintArr(AwgCol(arr2D));
+
+// Метод ввода данных пользователем
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод генерации и заполнение двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+// Метод вычисления среднего арифмитического по столбцам
+int[] AwgCol(int[,] arr)
+{
+    int[] awg = new int[arr.GetLength(1)];
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            awg[j] += arr[i, j];
+        }
+        awg[j] = awg[j] / arr.GetLength(0);
+    }
+    return awg;
+}
+
+// Метод печати цветного двумерного массива
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод вывода одномерного массива
+void PrintArr(int[] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+        Console.Write(arr[i] + "   ");
+        Console.ResetColor();
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
